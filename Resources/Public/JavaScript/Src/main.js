b28c2619cbe011a14f5bcb5aa0ca66311c41b357
aariasmora <alfedo.arias.mora@hof-university.de>
2018-02-13 23:25:24 +0100

    updated with car data ext files

--- Resources/Public/JavaScript/Src/main.js
+++ Resources/Public/JavaScript/Src/main.js
@@ -1,1 +1,45 @@
 console.log('WE LOVE TYPO3');
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTableBody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
+function sortTable(a) {
+    var table, rows, switching, i, x, y, a, shouldSwitch;
+    this.a = a;
+    table = document.getElementById("myTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("tr");
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("a")[this.a];
+            y = rows[i + 1].getElementsByTagName("a")[this.a];
+            //check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch= true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
